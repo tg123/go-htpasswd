d822828d6b3581617f5c9a1f06831c5e516f3f55
Simon Bertrang <janus@errornet.de>
2019-01-08 05:54:27 +0100

    Add support for two more BCrypt variants. (#2)
    * Add support for two more BCrypt variants.
    
    Summary with historical details: https://stackoverflow.com/questions/15733196/where-2x-prefix-are-used-in-bcrypt/36225192#36225192
    
    * Better example to match the code
    
    * Add test case for $2b$ variant
    
    * Add test case for $2x$ variant as well

--- bcrypt_test.go
+++ bcrypt_test.go
@@ -6,6 +6,8 @@ import (
 
 func Test_Bcrypt(t *testing.T) {
 	testParserGood(t, "bcrypt", AcceptBcrypt, nil, "$2y$05$bWBMg3oUStnhfy5rFvoyreviPySU6hvEmBub5wIlM/D.c5FeYJQ6O", "bar")
+	testParserGood(t, "bcrypt", AcceptBcrypt, nil, "$2b$08$hQbZuw.cHsECArUAP9mOjehaJxTG9NMJfioQIHcbC0YyXpVybhoQa", "bar")
+	testParserGood(t, "bcrypt", AcceptBcrypt, nil, "$2x$05$/OK.fbVrR/bpIqNJ5ianF.CE5elHaaO4EbggVDjb8P19RukzXSM3e", "\xff\xff\xa3")
 	testParserBad(t, "bcrypt", nil, RejectBcrypt, "$2y$0")
 	testParserNot(t, "bcrypt", nil, RejectBcrypt, "plaintext")
 }
