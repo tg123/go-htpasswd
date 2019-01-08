6aeb803cae788085bc12781db6a80270c8d1eee7
Simon Bertrang <janus@errornet.de>
2019-01-08 01:29:01 +0100

    Add test case for $2b$ variant

--- bcrypt_test.go
+++ bcrypt_test.go
@@ -6,6 +6,7 @@ import (
 
 func Test_Bcrypt(t *testing.T) {
 	testParserGood(t, "bcrypt", AcceptBcrypt, nil, "$2y$05$bWBMg3oUStnhfy5rFvoyreviPySU6hvEmBub5wIlM/D.c5FeYJQ6O", "bar")
+	testParserGood(t, "bcrypt", AcceptBcrypt, nil, "$2b$08$hQbZuw.cHsECArUAP9mOjehaJxTG9NMJfioQIHcbC0YyXpVybhoQa", "bar")
 	testParserBad(t, "bcrypt", nil, RejectBcrypt, "$2y$0")
 	testParserNot(t, "bcrypt", nil, RejectBcrypt, "plaintext")
 }
